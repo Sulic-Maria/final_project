155125826cdeaa3940476e1211a9cc2145edbb47
Sulic Maria <sulicmaria.alexandra@gmail.com>
2023-10-15 17:31:48 +0300

    Changed the products.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML += `
-  <div>
-    <h2>Maria Sulic</h2>
-  </div>
-`;
+window.addEventListener("load", showProducts);
+async function showProducts() {
+  console.log("Page fully loaded");
+  var response = await fetch(
+    "https://652bdb87d0d1df5273eecf72.mockapi.io/services"
+  );
+  var products = await response.json();
+  console.log(products);
+  var productsDiv = document.getElementById("products");
+  var productsHTML = "";
+  for (var i = 0; i <= products.length - 1; i++) {
+    productsHTML += `<div class ="card"> <img src=${products[i].imageURL}/>
+    </div>`;
+  }
+  productsDiv.innerHTML = productsHTML;
+}
